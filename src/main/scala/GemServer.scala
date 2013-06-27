aa66303b5bfbc54b6bcbf32ff03a3db70ee733fb
Brad Fritz <brad@fewerhassles.com>
2013-06-27 10:08:04 -0400

    first cut at server to receive GEM HTTP-style messages
    Starts server on port 8089 (or value in $PORT environment
    variable) and echo GEM serial number everytime a message
    is received.  Assumes GEM is already configured to use
    HTTP-style message format.

---
+++ src/main/scala/GemServer.scala
@@ -0,0 +1,59 @@
+package com.bfritz.rockcollector
+
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.{
+  ChannelHandlerContext,
+  ChannelInitializer,
+  SimpleChannelInboundHandler}
+import io.netty.channel.socket.SocketChannel
+import io.netty.channel.socket.nio.NioServerSocketChannel
+import io.netty.handler.codec.http.{
+  HttpRequest,
+  HttpRequestDecoder,
+  QueryStringDecoder}
+import io.netty.handler.logging.{LoggingHandler, LogLevel}
+
+import java.net.InetSocketAddress
+
+/**
+ * Netty server that listens for updates from the GEM via TCP socket.
+ * Loosely based on Netty
+ * <a href="https://github.com/netty/netty/blob/master/example/src/main/java/io/netty/example/telnet/TelnetServer.java">TelnetServer</a>
+ * example.
+ */
+object GemServer extends App {
+  val port = Option(System.getenv("PORT")).map(_.toInt).getOrElse(8089)
+  start(port)
+
+  def start(port: Int) {
+    val bossGroup, workerGroup = new NioEventLoopGroup()
+    try {
+      val socket = new InetSocketAddress(port)
+      new ServerBootstrap()
+        .group(bossGroup, workerGroup)
+        .channel(classOf[NioServerSocketChannel])
+        .handler(new LoggingHandler(LogLevel.INFO))
+        .childHandler(new GemServerInitializer())
+        .bind(socket).sync().channel().closeFuture().sync()
+    } finally {
+        bossGroup.shutdownGracefully()
+        workerGroup.shutdownGracefully()
+    }
+  }
+
+  class GemServerInitializer extends ChannelInitializer[SocketChannel] {
+    override def initChannel(ch: SocketChannel) {
+      ch.pipeline()
+        .addLast("decoder", new HttpRequestDecoder())
+        .addLast("handler", new GemServerHandler())
+    }
+  }
+
+  class GemServerHandler extends SimpleChannelInboundHandler[HttpRequest] {
+    override def messageReceived(ctx: ChannelHandlerContext, req: HttpRequest) {
+      val qs = new QueryStringDecoder(req.getUri)
+      val serialNo = qs.parameters.get("SN").get(0)
+    }
+  }
+}
