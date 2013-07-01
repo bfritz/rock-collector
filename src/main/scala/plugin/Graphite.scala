dc2789c53fa19c8fd86f36627c246f54f168e351
Brad Fritz <brad@fewerhassles.com>
2013-07-01 14:11:24 -0400

    publish watt/second counters to Graphite

--- src/main/scala/plugin/Graphite.scala
+++ src/main/scala/plugin/Graphite.scala
@@ -1,5 +1,7 @@
 package com.bfritz.rockcollector.plugin
 
+import com.bfritz.rockcollector.GemServer.ChannelRegex
+
 import com.bfritz.rockcollector.event.GemEvent
 
 import com.google.common.base.Charsets.UTF_8
@@ -7,7 +9,7 @@ import com.google.common.eventbus.{EventBus, Subscribe}
 
 import com.typesafe.scalalogging.slf4j.Logging
 
-import java.io.{BufferedWriter, OutputStreamWriter}
+import java.io.{BufferedWriter, OutputStreamWriter, Writer}
 import java.net.InetSocketAddress
 
 import javax.net.SocketFactory
@@ -31,13 +33,23 @@ class Graphite(bus: EventBus) extends GemPlugin with Logging {
 
   @Subscribe
   def onMessage(event: GemEvent) {
+    val ts: Long = System.currentTimeMillis / 1000L
     for (s <- managed(socketFactory.createSocket(graphite.getAddress, graphite.getPort));
         w <- managed(new BufferedWriter(new OutputStreamWriter(s.getOutputStream, UTF_8)))) {
-      val secs: Long = System.currentTimeMillis / 1000L
-      val line = s"greeneye.${event.sn}.foo 0.123 $secs\n"
-      logger.debug(line)
-      w.write(line)
-      w.flush()
+      for ((k, v) <- event.values) {
+        k match {
+          case ChannelRegex(chNo) =>
+            send(w, event, ts, s"channel.$chNo", v)
+          case _ =>
+        }
+      }
+      send(w, event, ts, "volts", event.values("volts"))
     }
+  }
+
+  def send(w: Writer, event: GemEvent, seconds: Long, metric: String, value: Number) {
+    val line = s"greeneye.${event.sn}.$metric $value $seconds\n"
+    logger.trace(line.trim)
+    w.write(line)
   }
 }
